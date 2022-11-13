a6e347214d41ec670a7b9e1d4c8e8e52d2c55ea8
Jennifer Zajac <zajac.je@northeastern.edu>
2022-11-12 20:33:32 -0500

    hello world

---
+++ app.js
@@ -0,0 +1,7 @@
+import express from 'express';
+const app = express();
+app.get("/hello", (req, res) => {
+  res.send("Life is good!");
+});
+app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+app.listen(4000);
