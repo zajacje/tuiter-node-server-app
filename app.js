628babdfa2b948f27f492a5fa307308ce600c2ee
Jennifer Zajac <zajac.je@northeastern.edu>
2022-11-12 21:43:33 -0500

    A8 exercises

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-import express from 'express';
+import express from "express";
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users/users-controller.js";
 const app = express();
-app.get("/hello", (req, res) => {
-  res.send("Life is good!");
-});
-app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+app.use(express.json());
+HelloController(app);
+UserController(app);
 app.listen(4000);
